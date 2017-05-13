7c5e8bfc8134aa16a2d6cfd796754f81166e5cca
advancingKoala <milaiduoduo4567@163.com>
2017-05-13 13:03:46 +0800

    delete ejs_test1 file

--- ejs_test1/index.js
+++
@@ -1,9 +0,0 @@
-var data={buyflag:true,
-    things1:["apple","pineapple","pen"],
-    things2:["nothing","nothing","nothing"]
-};
-
-window.onload=function(){
-    var html=new EJS({url:'./model1.ejs'}).render(data);
-    document.getElementById("container").innerHTML=html;
-}
